64e3513743b57be774593fdcf175b2833aae2021
jlin96 <Jesse13234@gmail.com>
2024-07-25 17:40:27 -0400

    added user model

---
+++ inventory-management/src/main/java/com/skillstorm/inventory_management/models/User.java
@@ -0,0 +1,71 @@
+package com.skillstorm.inventory_management.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(length = 50, name = "first_name")
+    private String firstName;
+
+    @Column(length = 50, name = "last_name")
+    private String lastName;
+
+    @Column(length = 50)
+    private String email;
+
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    
+
+}
