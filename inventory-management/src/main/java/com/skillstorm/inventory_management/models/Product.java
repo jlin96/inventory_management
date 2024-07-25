d3ac99c4cdebe456e8d8081e14aa1afa3746037c
jlin96 <Jesse13234@gmail.com>
2024-07-25 16:39:43 -0400

    product/warehouse model done

---
+++ inventory-management/src/main/java/com/skillstorm/inventory_management/models/Product.java
@@ -0,0 +1,67 @@
+package com.skillstorm.inventory_management.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(length = 50, nullable = false)
+    private String name;
+
+    @Column(length = 75)
+    private String description;
+
+    @Column(name = "stock_amount")
+    private int stockAmount;
+
+    public Product(int id, String name, String description, int stockAmount) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.stockAmount = stockAmount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getStockAmount() {
+        return stockAmount;
+    }
+
+    public void setStockAmount(int stockAmount) {
+        this.stockAmount = stockAmount;
+    }
+
+    
+}
---
+++ inventory-management/src/main/java/com/skillstorm/inventory_management/models/Warehouse.java
@@ -0,0 +1,55 @@
+package com.skillstorm.inventory_management.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="warehouse")
+public class Warehouse {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(length = 50)
+    private String name;
+
+    @Column(length = 50, nullable = false)
+    private String address;
+
+    public Warehouse(int id, String name, String address) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    
+}
