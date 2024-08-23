b920bb76f9c38ca9dd50c5c9a7b3bb157bab03f9
Reynaldo Silva <reynaldo.silva0101@gmail.com>
2024-08-23 19:09:20 -0400

    testing ci/cd

--- inventory-management/src/test/java/com/skillstorm/inventory_management/selenium/Product.java
+++ inventory-management/src/test/java/com/skillstorm/inventory_management/selenium/Product.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+// comment
 public class Product {
     
     private WebDriver driver;
