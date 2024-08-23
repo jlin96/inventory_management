c72d120d5b9b4443e2bb855f61a212eb2f636361
Reynaldo Silva <reynaldo.silva0101@gmail.com>
2024-08-23 19:37:51 -0400

    ci/cd test

--- inventory-management/src/main/java/com/skillstorm/inventory_management/InventoryManagementApplication.java
+++ inventory-management/src/main/java/com/skillstorm/inventory_management/InventoryManagementApplication.java
@@ -8,7 +8,7 @@ public class InventoryManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryManagementApplication.class, args);
-		System.out.println("Hello World!");
+		System.out.println("Hello World! test");
 	}
 
 }
